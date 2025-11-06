7305d23a98fdedc58d7ffdbcbe60a84b3911cd21
Unknown <unknown@example.com>
2025-11-06 23:10:53 +0800

    Add SignUp and Login
    SignUp跟Login名稱對調

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import AppHeader from './components/AppHeader.vue'
-import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
 const featuredContent = ref({
     title: '刺客伍陸柒 feat. 雞大寶',
     rating: '8.7',
@@ -13,7 +13,6 @@ const featuredContent = ref({
 
 <template>
     <div class="min-h-screen bg-[#757575] text-white font-sans overflow-x-hidden">
-        <Login />
-        <AppHeader />
+        <SignUp />
     </div>
 </template>
